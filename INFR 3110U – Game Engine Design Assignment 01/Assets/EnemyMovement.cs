fcd16055c1fd66fc7c3a3d461580dca5c10ed0d4
James Pham <james.pham2@ontariotechu.net>
2021-09-30 19:44:13 -0400

    Assignment 1

---
+++ INFR 3110U – Game Engine Design Assignment 01/Assets/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private GameObject player;
+    public float Speed = 6.0f;
+
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        transform.LookAt(player.transform);
+        transform.position += transform.forward * Speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //GameObject.Destroy(collision.gameObject);
+        }
+    }
+}
